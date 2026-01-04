c64e3cb9fabf49c0762ecbc25a18631b81501bae
dev.Rodrigo Niskier <niskier.rodrigo@gmail.com>
2026-01-04 07:26:34 -0300

    Add files via upload

---
+++ components/ui/Icons.tsx
@@ -0,0 +1,39 @@
+import {
+  BookOpen,
+  Video,
+  FileText,
+  Mic,
+  MonitorPlay,
+  CheckCircle,
+  Award,
+  Settings,
+  Plus,
+  Trash2,
+  Edit,
+  ArrowRight,
+  ArrowLeft,
+  Lock,
+  Download,
+  Printer,
+  X
+} from 'lucide-react';
+
+export {
+  BookOpen,
+  Video,
+  FileText,
+  Mic,
+  MonitorPlay,
+  CheckCircle,
+  Award,
+  Settings,
+  Plus,
+  Trash2,
+  Edit,
+  ArrowRight,
+  ArrowLeft,
+  Lock,
+  Download,
+  Printer,
+  X
+};

e637bc3985365ffc43840745bebc08098b097e71
dev.Rodrigo Niskier <niskier.rodrigo@gmail.com>
2026-01-07 10:51:02 -0300

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,18 +1,11 @@
 import { Course } from './types';
 
 export const ADMIN_PIN = '1517';
-// constants.ts
-
-import { Course } from './types';
 
 // Toda vez que você atualizar os cursos no código, AUMENTE este número (1, 2, 3...)
-// Isso forçará o navegador dos alunos a baixar os novos cursos.
 export const DATA_VERSION = 2; 
 
 export const INITIAL_COURSES: Course[] = [
-  // AQUI VOCÊ COLARÁ O CONTEÚDO EXPORTADO DO ADMIN
-  // Por enquanto, mantenha o array vazio ou com os dados de exemplo que já tiver
-  [
   {
     "id": "1767791811245",
     "title": "História da Autorrevelação Especial de Deus",
@@ -52,5 +45,4 @@ export const INITIAL_COURSES: Course[] = [
       }
     ]
   }
-]
 ];

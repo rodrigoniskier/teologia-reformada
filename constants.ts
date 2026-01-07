8c62dbcdbe83cbf9afcfbe31d4a4ceaafdabb250
dev.Rodrigo Niskier <niskier.rodrigo@gmail.com>
2026-01-07 10:01:38 -0300

    Add DATA_VERSION and modify INITIAL_COURSES
    Added DATA_VERSION constant and updated INITIAL_COURSES.

--- constants.ts
+++ constants.ts
@@ -1,6 +1,18 @@
 import { Course } from './types';
 
 export const ADMIN_PIN = '1517';
+// constants.ts
+
+import { Course } from './types';
+
+// Toda vez que você atualizar os cursos no código, AUMENTE este número (1, 2, 3...)
+// Isso forçará o navegador dos alunos a baixar os novos cursos.
+export const DATA_VERSION = 1; 
+
+export const INITIAL_COURSES: Course[] = [
+  // AQUI VOCÊ COLARÁ O CONTEÚDO EXPORTADO DO ADMIN
+  // Por enquanto, mantenha o array vazio ou com os dados de exemplo que já tiver
+];
 
 export const INITIAL_COURSES: Course[] = [
   {
